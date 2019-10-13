a77b45afb3cca41b6cbc5d39b5a007ff7d55bef9
remysucre <remy.sucre@gmail.com>
2019-10-13 16:03:52 -0700

    basic grammar

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
-use smallvec::smallvec;
+use std::collections::HashSet;
 
 #[derive(Parser)]
 #[grammar = "hop.pest"]
@@ -125,157 +125,46 @@ type Constant = NotNan<f64>;
 define_term! {
     #[derive(Debug, PartialEq, Eq, Hash, Clone)]
     pub enum Math {
-        Constant(Constant),
         Add = "+",
-        Sub = "-",
         Mul = "*",
-        Div = "/",
-        Pow = "pow",
-        Exp = "exp",
-        Log = "log",
-        Sqrt = "sqrt",
-        Cbrt = "cbrt",
-        Fabs = "fabs",
-        // Sin = "sin",
-        // Cos = "cos",
-        // Tan = "tan",
-        // Asin = "asin",
-        // Acos = "acos",
-        // Atan = "atan",
-        // Atan2 = "atan2",
-        // Sinh = "sinh",
-        // Cosh = "cosh",
-        // Tanh = "tanh",
-        // Asinh = "asinh",
-        // Acosh = "acosh",
-        // Atanh = "atanh",
+        Div = "SUM",
 
-        // Fma = "fma",
-        Log1p = "log1p",
-        Expm1 = "expm1",
-        // Hypot = "hypot",
-
-        // PositAdd = "+.p16",
-        // PositSub = "-.p16",
-        // PositMul = "*.p16",
-        // PositDiv = "/.p16",
-        RealToPosit = "real->posit",
+        Dim = "dim",
+        Matrix = "b+",
+        Constant(Constant),
         Variable(Name),
     }
 }
 
 impl Language for Math {
     fn cost(&self, children: &[u64]) -> u64 {
-        let cost = match self {
-            Math::Constant(_) | Math::Variable(_) => 1,
-            Math::Add => 40,
-            Math::Sub => 40,
-            Math::Mul => 40,
-            Math::Div => 40,
-            Math::Pow => 210,
-            Math::Exp => 70,
-            Math::Log => 70,
-            Math::Sqrt => 40,
-            Math::Cbrt => 80,
-            Math::Fabs => 40,
-            Math::RealToPosit => 0,
-            Math::Expm1 => 70,
-            Math::Log1p => 70,
-        };
-
+        let cost = 1;
         cost + children.iter().sum::<u64>()
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Meta {
-    pub cost: u64,
-    pub best: RecExpr<Math>,
+    pub schema: HashSet<Name>,
 }
 
 fn eval(op: Math, args: &[Constant]) -> Option<Constant> {
-    let a = |i| args.get(i).cloned();
-    match op {
-        Math::Add => Some(a(0)? + a(1)?),
-        Math::Sub => Some(a(0)? - a(1)?),
-        Math::Mul => Some(a(0)? * a(1)?),
-        Math::Div => Some(a(0)? / a(1)?),
-        Math::Pow => None, // a(0)?.powf(a(1)?),
-        Math::Exp => None, // a(0)?.exp(),
-        Math::Log => None, // a(0)?.ln(),
-        Math::Sqrt => {
-            None
-            // unimplemented!()
-            // if let Some(sqrt) = args[0].sqrt() {
-            //     #[allow(clippy::float_cmp)]
-            //     let is_int = sqrt == sqrt.trunc();
-            //     if is_int {
-            //         sqrt.into()
-            //     } else {
-            //         None
-            //     }
-            // } else {
-            //     None
-            // }
-        }
-        // Math::Cbrt => {
-        //     if let Some(cbrt) = args[0].to_f64().map(f64::cbrt) {
-        //         #[allow(clippy::float_cmp)]
-        //         let is_int = cbrt == cbrt.trunc();
-        //         if is_int {
-        //             cbrt.into()
-        //         } else {
-        //             None
-        //         }
-        //     } else {
-        //         None
-        //     }
-        // }
-        Math::Fabs => Some(Constant::new(args[0].abs()).unwrap()),
-        Math::RealToPosit => Some(args[0]),
-        _ => None,
-    }
+    None
 }
 
+// TODO
 impl egg::egraph::Metadata<Math> for Meta {
     type Error = std::convert::Infallible;
     fn merge(&self, other: &Self) -> Self {
-        if self.cost <= other.cost {
-            self.clone()
-        } else {
-            other.clone()
-        }
+        self.clone()
     }
 
     fn make(expr: Expr<Math, &Self>) -> Self {
-        let expr = {
-            let const_args: Option<Vec<Constant>> = expr
-                .children
-                .iter()
-                .map(|meta| match meta.best.as_ref().op {
-                    Math::Constant(c) => Some(c),
-                    _ => None,
-                })
-                .collect();
-
-            const_args
-                .and_then(|a| eval(expr.op.clone(), &a))
-                .map(|c| Expr::unit(Math::Constant(c)))
-                .unwrap_or(expr)
-        };
-
-        let best: RecExpr<_> = expr.map_children(|c| c.best.clone()).into();
         Self {
-            best,
-            cost: expr.map_children(|c| c.cost).cost(),
+            schema: HashSet::default()
         }
     }
 
     fn modify(eclass: &mut EClass<Math, Self>) {
-        // NOTE pruning vs not pruning is decided right here
-        let best = eclass.metadata.best.as_ref();
-        if best.children.is_empty() {
-            eclass.nodes = vec![Expr::unit(best.op.clone())]
-        }
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -14,80 +14,11 @@ use wopt::{Math, Meta, parse_hop, parse_hop_file};
 use std::collections::HashMap;
 
 #[test]
-fn hop_parse(){ 
-  println!("{:?}", parse_hop_file("/Users/remywang/wormhole/wopt/tests/hops"));
+fn hop_parse(){
+  println!("{:?}", parse_hop_file("/Users/r/wormhole/wopt/tests/hops"));
 }
 
 static HOP:&str ="101;395;op;394,378;0,0,-1,-1,-1; ...";
-
-static HOPS:&str = "(359);u(print);;[-1,-1,-1,-1,-1]; [-,0,0 -> 0MB]
-(388);u(print);;[-1,-1,-1,-1,-1]; [-,0,0 -> 0MB]
-(385);TRead y;;[10000,1,1000,1000,10000]; [0,0,0 -> 0MB]
-(389);ua(+RC);(385);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(391);b(/);(389);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(514);b(+);(391);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(394);b(^);(385);[10000,1,1000,1000,10000]; [0,0,0 -> 0MB]
-(395);ua(+RC);(394);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(399);b(^);(391);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(400);b(*);(399);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(401);b(-);(395,400);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(406);b(/);(401);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(516);u(sqrt);(406);[-1,-1,-1,-1,-1]; [0,0,0 -> 0MB]
-(517);b(+);(516);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(518);b(cbind);(514,517);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(384);TRead X;;[10000,1000,1000,1000,9001506]; [0,0,18205 -> 18205MB]
-(379);TRead beta_unscaled;;[1000,1,1000,1000,-1]; [0,0,18205 -> 18205MB]
-(408);ba(+*);(384,379);[10000,1,1000,1000,-1]; [36409,0,0 -> 36409MB]
-(409);b(-);(385,408);[10000,1,1000,1000,-1]; [0,0,0 -> 0MB]
-(410);ua(+RC);(409);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(412);b(/);(410);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(520);b(+);(412);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(521);b(cbind);(518,520);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(415);b(^);(409);[10000,1,1000,1000,-1]; [0,0,0 -> 0MB]
-(416);ua(+RC);(415);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(420);b(^);(412);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(421);b(*);(420);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(422);b(-);(416,421);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(466);b(/);(422);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(523);u(sqrt);(466);[-1,-1,-1,-1,-1]; [0,0,0 -> 0MB]
-(524);b(+);(523);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(525);b(cbind);(521,524);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(432);b(/);(416);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(434);t(ifelse);(432);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(527);b(+);(434);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(528);b(cbind);(525,527);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(425);b(/);(416,401);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(426);b(-);(425);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(530);b(+);(426);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(531);b(cbind);(528,530);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(443);b(/);(434,406);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(444);b(-);(443);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(446);t(ifelse);(444);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(533);b(+);(446);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(534);b(cbind);(531,533);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(449);b(/);(422,401);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(450);b(-);(449);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(536);b(+);(450);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(537);b(cbind);(534,536);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(472);b(/);(466,406);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(473);b(-);(472);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(539);b(+);(473);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(540);b(cbind);(537,539);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(495);b(/);(416,395);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(496);b(-);(495);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(550);b(+);(496);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(551);b(cbind);(540,550);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(507);b(/);(395);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(508);b(/);(432,507);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(509);b(-);(508);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(511);t(ifelse);(509);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(553);b(+);(511);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(554);b(cbind);(551,553);[0,0,-1,-1,-1]; [0,0,0 -> 0MB]
-(569);u(print);(554);[-1,-1,-1,-1,-1]; [0,0,0 -> 0MB]
-(571);u(print);;[-1,-1,-1,-1,-1]; [-,0,0 -> 0MB]
-(590);PWrite beta_out;(379);[1000,1,-1,-1,-1]; [18204,0,18205 -> 36409MB]
-(604);u(print);;[-1,-1,-1,-1,-1]; [-,0,0 -> 0MB]
-";
 
 #[test]
 fn wopt() {
@@ -114,24 +45,27 @@ fn wopt() {
     let mut eg: EGraph<Math, Meta> = EGraph::default();
 
     from_dag(&mut eg, &hop);
-    //let mut id_map: HashMap<Id, Id> = HashMap::new();
-
-    //let (id, exp) = x_id;
-    //let node = exp.map_children(|child| *id_map.get(&child).unwrap());
-    //let new_id = eg.add(node).id;
-    //id_map.insert(id, new_id);
-
-    //let (id, exp) = y_id;
-    //let node = exp.map_children(|child| *id_map.get(&child).unwrap());
-    //let new_id = eg.add(node).id;
-    //id_map.insert(id, new_id);
-
-    //let (id, exp) = plus_id;
-    //let node = exp.map_children(|child| *id_map.get(&child).unwrap());
-    //let new_id = eg.add(node).id;
-    //id_map.insert(id, new_id);
-
     eg.dump_dot("wopt.dot");
+}
+
+#[test]
+fn grammar() {
+    let start = "(SUM (dim j 10) (+ a b))";
+    let start_expr = Math::parse_expr(start).unwrap();
+
+    let (mut egraph, _root) = EGraph::<Math, ()>::from_expr(&start_expr);
+
+    //let rules = wopt::rules();
+
+    for _ in 0..4 {
+        for (_name, rs) in wopt::rules() {
+            for rule in rs {
+                rule.run(&mut egraph);
+                egraph.rebuild();
+            }
+        }
+    }
+    egraph.dump_dot("grammar.dot");
 }
 
 fn from_dag(eg: &mut EGraph<Math, Meta>, dag: &[(Id, Expr<Math, Id>)]) {
@@ -289,134 +223,4 @@ fn does_not_simplify() {
         limit: 1_000,
     }
     .check();
-}
-
-#[test]
-fn simplifies() {
-    CheckSimplify {
-        start: r#"
-          (/ 1
-             (- (/ (+ 1 (sqrt five))
-                   2)
-                (/ (- 1 (sqrt five))
-                   2)))
-        "#,
-        end: "(/ 1 (sqrt five))",
-        iters: 6,
-        limit: 75_000,
-    }
-    .check();
-}
-
-#[test]
-fn fold_after_rewrite() {
-    CheckSimplify {
-        start: "
-          (+ 1
-             (- a
-                (* (- 2 1)
-                   a)))",
-        end: "1",
-        iters: 4,
-        limit: 10_000,
-    }
-    .check();
-}
-
-static EXP: &str = r#"
-(/
- (*
-  (*
-   (*
-    (pow
-     (/ 1 (+ 1 (exp (- 0 s))))
-     c_p)
-    (pow
-     (- 1 (/ 1 (+ 1 (exp (- 0 s)))))
-     c_n))
-   (*
-    (pow
-     (/ 1 (+ 1 (exp (- 0 s))))
-     c_p)
-    (pow
-     (- 1 (/ 1 (+ 1 (exp (- 0 s)))))
-     c_n)))
-  (*
-   (pow
-    (/ 1 (+ 1 (exp (- 0 s))))
-    c_p)
-   (pow
-    (- 1 (/ 1 (+ 1 (exp (- 0 s)))))
-    c_n)))
- (*
-  (*
-   (*
-    (pow
-     (/ 1 (+ 1 (exp (- 0 t))))
-     c_p)
-    (pow
-     (- 1 (/ 1 (+ 1 (exp (- 0 t)))))
-     c_n))
-   (*
-    (pow
-     (/ 1 (+ 1 (exp (- 0 t))))
-     c_p)
-    (pow
-     (- 1 (/ 1 (+ 1 (exp (- 0 t)))))
-     c_n)))
-  (*
-   (pow
-    (/ 1 (+ 1 (exp (- 0 t))))
-    c_p)
-   (pow
-    (- 1 (/ 1 (+ 1 (exp (- 0 t)))))
-    c_n))))
-"#;
-
-#[test]
-fn do_something() {
-    let _ = env_logger::builder().is_test(true).try_init();
-    let start_expr = Math::parse_expr(EXP).unwrap();
-    let (mut egraph, root) = EGraph::<Math, Meta>::from_expr(&start_expr);
-
-    let herbies_result = "(*
-  (*
-   (*
-    (/
-     (pow (- 1 (/ 1 (+ (exp (- 0 s)) 1))) c_n)
-     (pow (- 1 (/ 1 (+ (exp (- 0 t)) 1))) c_n))
-    (/ (pow (/ 1 (+ (exp (- 0 s)) 1)) c_p) (pow (/ 1 (+ (exp (- 0 t)) 1)) c_p)))
-   (*
-    (/
-     (pow (- 1 (/ 1 (+ (exp (- 0 s)) 1))) c_n)
-     (pow (- 1 (/ 1 (+ (exp (- 0 t)) 1))) c_n))
-    (/ (pow (/ 1 (+ (exp (- 0 s)) 1)) c_p) (pow (/ 1 (+ (exp (- 0 t)) 1)) c_p))))
-  (*
-   (/
-    (pow (- 1 (/ 1 (+ (exp (- 0 s)) 1))) c_n)
-    (pow (- 1 (/ 1 (+ (exp (- 0 t)) 1))) c_n))
-   (/ (pow (/ 1 (+ (exp (- 0 s)) 1)) c_p) (pow (/ 1 (+ (exp (- 0 t)) 1)) c_p))))";
-
-    let other_expr = Math::parse_expr(herbies_result).unwrap();
-    println!(
-        "Herbie ({}): {}",
-        calculate_cost(&other_expr),
-        other_expr.to_sexp()
-    );
-
-    run_rules(&mut egraph, 3, 20_000);
-    let start_time = Instant::now();
-
-    let ext = Extractor::new(&egraph);
-    let best = ext.find_best(root);
-    let extract_time = start_time.elapsed();
-
-    println!(
-        "Start ({}): {}",
-        calculate_cost(&start_expr),
-        start_expr.to_sexp()
-    );
-    println!("Best ({}): {}", best.cost, best.expr.to_sexp());
-
-    println!("Extract time: {:.4}", extract_time.as_secs_f64());
 }
